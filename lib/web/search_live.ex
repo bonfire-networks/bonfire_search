3888acb9d5c8a79f0d6ab0529323537de679c839
Mayel de Borniol <git@mayel.space>
2022-04-30 21:19:12 +1200

    s

--- lib/web/search_live.ex
+++ lib/web/search_live.ex
@@ -36,19 +36,16 @@ defmodule Bonfire.Search.Web.SearchLive do
   end
 
   def handle_params(%{"s" => s, "facet" => facets} = _params, _url, socket) when s != "" do
-
     Bonfire.Search.LiveHandler.live_search(s, 20, facets, socket |> assign_global(search_more: true))
   end
 
 
   def handle_params(%{"s" => s} = _params, _url, socket) when s != "" do
-
     Bonfire.Search.LiveHandler.live_search(s, socket |> assign_global(search_more: true))
   end
 
   def handle_params(%{"hashtag_search" => s} = _params, _url, socket) when s != "" do
-
-    Bonfire.Search.LiveHandler.live_search(s, socket |> assign_global(search_more: true))
+    Bonfire.Search.LiveHandler.live_search("\"#{s}\"", socket |> assign_global(search_more: true))
   end
 
   def handle_params(_params, _url, socket) do
