1d2e81d2756092d92f6413e9a078d54e9d8c6660
Mayel de Borniol <git@mayel.space>
2022-06-14 09:04:17 +1200

    Use compile_env

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -33,7 +33,7 @@ defmodule Bonfire.Search.ConnCase do
       alias Bonfire.Search.Web.Router.Helpers, as: Routes
 
       # The default endpoint for testing
-      @endpoint Bonfire.Common.Config.get!(:endpoint_module)
+      @endpoint Application.compile_env!(:bonfire, :endpoint_module)
     end
   end
 
