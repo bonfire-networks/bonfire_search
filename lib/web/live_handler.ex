a82db7cf49418e9779e4eeaa49fa3c52c182db0e
Mayel de Borniol <git@mayel.space>
2022-10-29 23:29:28 +1300

    fedi

--- lib/web/live_handler.ex
+++ lib/web/live_handler.ex
@@ -94,7 +94,7 @@ defmodule Bonfire.Search.LiveHandler do
     # FIXME: use maybe_apply
     hits =
       (with {:ok, federated_object_or_character} <-
-              Bonfire.Federate.ActivityPub.Utils.get_by_url_ap_id_or_username(q) do
+              Bonfire.Federate.ActivityPub.AdapterUtils.get_by_url_ap_id_or_username(q) do
          [federated_object_or_character] ++
            (hits || [])
        else
