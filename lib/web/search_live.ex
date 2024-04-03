7e92a828af88f0bc937dc3bde87e579f7eecc0f6
Mayel de Borniol <git@mayel.space>
2024-04-03 14:08:09 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/web/search_live.ex
+++ lib/web/search_live.ex
@@ -6,7 +6,7 @@ defmodule Bonfire.Search.Web.SearchLive do
   @default_limit 20
 
   declare_extension("Search",
-    icon: "twemoji:magnifying-glass-tilted-left",
+    icon: "heroicons-solid:search",
     emoji: "🔍",
     description: l("Search for users or content."),
     exclude_from_nav: true
