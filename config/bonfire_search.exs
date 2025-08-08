1837fb787b4cf9be652898cb31fa69a7ff3f1140
Giacomo Leidi <goodoldpaul@autistici.org>
2025-08-08 20:16:10 +0200

    Allow reading meilisearch master key secret from file.

--- config/bonfire_search.exs
+++ config/bonfire_search.exs
@@ -7,7 +7,7 @@ config :bonfire_search,
   # protocol, hostname and port
   instance: System.get_env("SEARCH_MEILI_INSTANCE", "http://search:7700"),
   # secret key
-  api_key: System.get_env("MEILI_MASTER_KEY", "make-sure-to-change-me")
+  api_key: System.get_env("MEILI_MASTER_KEY") || File.read!(System.get_env("MEILI_MASTER_KEY_FILE"))
 
 # for use by API client
 # config :tesla, :adapter, {Tesla.Adapter.Finch, name: Bonfire.Finch}
