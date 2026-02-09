345e431bbdfbfdac4b5bf0786708ee015c71347f
ivan <ivanminutillo@outlook.it>
2026-02-09 15:53:47 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/916

--- lib/web/masto_search_controller.ex
+++ lib/web/masto_search_controller.ex
@@ -3,13 +3,9 @@ if Application.compile_env(:bonfire_api_graphql, :modularity) != :disabled do
     @moduledoc "Mastodon-compatible v2 search REST endpoint."
 
     use Bonfire.UI.Common.Web, :controller
-    import Untangle
 
     alias Bonfire.Search.API.GraphQLMasto.Adapter
 
-    def search(conn, params) do
-      debug(params, "GET /api/v2/search")
-      Adapter.search(params, conn)
-    end
+    def search(conn, params), do: Adapter.search(params, conn)
   end
 end
